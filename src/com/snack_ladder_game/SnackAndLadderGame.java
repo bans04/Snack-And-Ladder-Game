6511b4c7af66b794232b2742ca5095b03bd808b7
bans04 <bansodednyanadeo@gmail.com>
2022-06-21 10:13:21 +0530

    Generate a random num from 0 to 6 for Snack & Ladder Game.

--- src/com/snack_ladder_game/SnackAndLadderGame.java
+++ src/com/snack_ladder_game/SnackAndLadderGame.java
@@ -1,12 +1,19 @@
 package com.snack_ladder_game;
 /*
  * -----------------------Snack & Ladder Game-----------------------
+ * -> Print Welcome message
+ * 
  * */
 
 public class SnackAndLadderGame {
+	public static int getRandomNum() {
+		int randomNum =  (int) ((Math.random() * 6) + 1);
+		return randomNum;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snack & Ladder Game..|");
 		int startPosition;
+		System.out.println(getRandomNum());
 	}
 }
