2493ea6cbad8abc08c18e7f8ae48bee1a20debb7
bans04 <bansodednyanadeo@gmail.com>
2022-06-21 10:34:08 +0530

    The player check for option whether he is stay at some position or moved ahead or moved behind.

--- src/com/snack_ladder_game/SnackAndLadderGame.java
+++ src/com/snack_ladder_game/SnackAndLadderGame.java
@@ -2,10 +2,16 @@ package com.snack_ladder_game;
 /*
  * -----------------------Snack & Ladder Game-----------------------
  * -> Print Welcome message
+ * -> Generate random Num from 1 to 6
+ * -> 
  * 
  * */
 
-public class SnackAndLadderGame {
+public class SnackAndLadderGame { 
+	static final int noPlay = 0;
+	static final int ladder = 1;
+	static final int snack = 2;
+	
 	public static int getRandomNum() {
 		int randomNum =  (int) ((Math.random() * 6) + 1);
 		return randomNum;
@@ -13,7 +19,26 @@ public class SnackAndLadderGame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snack & Ladder Game..|");
-		int startPosition;
-		System.out.println(getRandomNum());
+		int startPosition = 0;
+		//System.out.println(getRandomNum());
+		int dice = getRandomNum();
+		
+		int checkPlay = (int) Math.floor(Math.random()*10)%3;
+		switch (checkPlay) {
+		case noPlay :
+			System.out.println("Player stay same position..|");
+			break;
+			
+		case ladder :
+			startPosition = startPosition + dice;
+			System.out.println("Player Moved ahead..|");
+			break;
+
+		case snack:
+			startPosition = startPosition - dice;
+			System.out.println("Player Moved behind..|");
+			break;
+		}
+		System.out.println(startPosition);
 	}
 }
