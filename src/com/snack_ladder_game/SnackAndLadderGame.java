eda5992472ff588987e83b1effec37c79fa55d81
bans04 <bansodednyanadeo@gmail.com>
2022-06-21 10:02:15 +0530

    Snack & ladder game played with single player at start position 0.

---
+++ src/com/snack_ladder_game/SnackAndLadderGame.java
@@ -0,0 +1,12 @@
+package com.snack_ladder_game;
+/*
+ * -----------------------Snack & Ladder Game-----------------------
+ * */
+
+public class SnackAndLadderGame {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snack & Ladder Game..|");
+		int startPosition;
+	}
+}
